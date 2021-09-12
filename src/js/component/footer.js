5665c08aba7c8f7ca0969c08e36843ca1f4fd627
apolopino <apolo.pino@gmail.com>
2021-09-12 20:50:42 +0000

    footer fixed at bottom

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer
+		className="footer mt-auto py-3 text-center"
+		style={{
+			position: "fixed",
+			bottom: 0,
+			left: 0,
+			"text-align": "center!important",
+			width: "100%"
+		}}>
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by
 			{"Apolo Pino"} in
